f159d40fab7965d3c9184b3a6845fa90a60ab8ac
Xnehil <harvy.martinezm@pucp.edu.pe>
2024-11-13 19:30:29 -0500

    hosts

--- O4_Aplicacion/validaciones/app.py
+++ O4_Aplicacion/validaciones/app.py
@@ -27,4 +27,4 @@ def create_app():
 app = create_app()
 
 if __name__ == "__main__":
-    app.run(debug=True, port=int(os.environ.get("PORT", 5000)))
+    app.run(debug=True, host="0.0.0.0",port=int(os.environ.get("PORT", 5000)))
